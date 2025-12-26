1ebe72de23ab41ba0612f376413f983f4b312008
desty <dimonmikryukov@gmail.com>
2025-12-27 02:29:28 +0400

    add readme

--- config.py
+++ config.py
@@ -5,11 +5,14 @@ from pathlib import Path
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+EXTERNAL_URL = os.getenv("EXTERNAL_URL")
 
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN не найден в .env")
 
+if not EXTERNAL_URL:
+    raise ValueError("EXTERNAL_URL не найден в .env")
+
 # Константы путей
 TRACK_FILE = Path("time.json")
 RESUME_FILE = Path("resume.json")
-EXTERNAL_URL = "http://your-url-here.com/api"  # ЗАМЕНИ НА СВОЙ URL
